a0287cae6bccef3c90f2fa07500415ac7f732760
Aishani Mitra <66082466+Aishani2001@users.noreply.github.com>
2021-06-16 20:23:15 +0530

    Create constants.php

---
+++ configure/constants.php
@@ -0,0 +1,9 @@
+  <?php 
+    //Create Constants to Store Non Repeating Values. Constants are stored by putting CAPS letters.
+    define('LOCALHOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'food-order');
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    $db_select = mysqli_select_db($conn, DB_NAME) //Selecting Database. die will terminate process.
+?>
